1a4b572e2208846406ac5a808993e10a4a0dc095
erdykivanka <erdyk36@gmail.com>
2026-03-24 23:54:24 +0100

    poprawilam pliki txt w ktorych braklo spacji ktora powodwale niepoprawne wczytanie pliku i dodalm funkcje displaj orz maina

--- main.cpp
+++ main.cpp
@@ -16,4 +16,29 @@ class Sudoku{
                     file >> arr[i][j];
         }
 
+        void display()
+        {
+            for(int i = 0; i < 9; i++)
+            {
+                for(int j = 0; j < 9; j++)
+                {
+                    if(board[i][j]== 0)
+                        cout << "*";
+                    else
+                        cout<< board[i][j];
+                }
+                //po jednej linjce przeskakujemy w dol
+                cout << "\n";
+            }
+        }
+
 };
+
+int main()
+{
+    Sudoku sudoku;
+    sudoku.load_board("data/easy_solution.txt", sudoku.solution);
+    sudoku.load_board("data/easy_board.txt", sudoku.board);
+
+    sudoku.display();
+}
